a187fd5360123a528fbbf25d69ac30450b099339
Angenoise <maf-delacruz.student@ua.edu.ph>
2026-06-23 15:29:42 +0800

    Initial commit

---
+++ backend/documenttracker/settings.py
@@ -0,0 +1,3 @@
+"""Compatibility wrapper that exposes document_tracker.settings."""
+
+from document_tracker.settings import *  # noqa: F401,F403
---
+++ backend/documenttracker/wsgi.py
@@ -0,0 +1,3 @@
+"""Compatibility wrapper that exposes document_tracker.wsgi.application."""
+
+from document_tracker.wsgi import application  # noqa: F401
